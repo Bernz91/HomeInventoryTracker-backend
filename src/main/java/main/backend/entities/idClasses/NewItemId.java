41c6ab5584591f3e731079038dd1d4fe2ab64c3a
Bernz91 <samuel.bernadi91@gmail.com>
2023-07-12 11:59:12 +0800

    third commit

--- src/main/java/main/backend/entities/idClasses/NewItemId.java
+++ src/main/java/main/backend/entities/idClasses/NewItemId.java
@@ -6,10 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @NoArgsConstructor
 public class NewItemId implements Serializable {
-    @Getter @Setter private UUID userId;
+    @Getter @Setter private String userId;
     @Getter @Setter private String itemName;
 }
